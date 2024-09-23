2bc5d93b71f570cb69cc715e0f785be60df77598
Ivanio Junior <juni96526@gmail.com>
2024-09-23 16:44:16 -0300

    otimizacao do codigo, acresentacao de certificado e acresentacao de algumas animacao

--- js/script.js
+++ js/script.js
@@ -1,20 +1,28 @@
-const curso = document.querySelectorAll(".curso button");
+const curso = document.querySelectorAll(".formacao button");
 
 function ativarCurso(event) {
-  const curso = event.currentTarget;
-  const controls = curso.getAttribute("aria-controls");
-  const lista = document.getElementById(controls);
-
-  lista.classList.toggle("ativa");
-  const ativa = lista.classList.contains("ativa");
-  curso.setAttribute("aria-expanded", ativa);
+  const lista = document.getElementById(event.currentTarget.getAttribute("aria-controls")
+  );
+  const ativo = lista.classList.toggle("ativo");
+  event.currentTarget.setAttribute("aria-expanded", ativo);
 }
 
-function eventosCurso(curso) {
-  curso.addEventListener("click", ativarCurso);
-}
+curso.forEach((event) => {
+  event.addEventListener("click", ativarCurso);
+});
 
-curso.forEach(eventosCurso);
-
-// animacao // 
-new SimpleAnime()
+window.addEventListener('scroll', function() {
+  const fixedElement = document.getElementById('fixedElement');
+  const footer = document.querySelector('.footer');
+  
+  // Pega as posições do footer e do elemento fixo
+  const footerRect = footer.getBoundingClientRect();
+  const fixedElementRect = fixedElement.getBoundingClientRect();
+  
+  // Se o elemento fixo estiver se sobrepondo ao footer, "parar" ele
+  if (footerRect.top <= window.innerHeight) {
+      fixedElement.classList.add('stopped');
+  } else {
+      fixedElement.classList.remove('stopped');
+  }
+});
