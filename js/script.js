65611f0ee4b5b6098c108a849990a93d9cfb805b
Ivanio Junior <juni96526@gmail.com>
2024-11-30 17:03:02 -0300

    animacoes

--- js/script.js
+++ js/script.js
@@ -1,12 +1,38 @@
-const curso = document.querySelectorAll(".formacao button");
+function initAccordion() {
+  const accordionList = document.querySelectorAll(".js-formacao button");
+  if (accordionList.length) {
+    function activeAccordion(event) {
+      const lista = document.getElementById(
+        event.currentTarget.getAttribute("aria-controls")
+      );
+      const ativo = lista.classList.toggle("ativo");
+      event.currentTarget.setAttribute("aria-expanded", ativo);
+    }
 
-function ativarCurso(event) {
-  const lista = document.getElementById(event.currentTarget.getAttribute("aria-controls")
-  );
-  const ativo = lista.classList.toggle("ativo");
-  event.currentTarget.setAttribute("aria-expanded", ativo);
+    accordionList.forEach((event) => {
+      event.addEventListener("click", activeAccordion);
+    });
+  }
 }
+initAccordion();
 
-curso.forEach((event) => {
-  event.addEventListener("click", ativarCurso);
-});
+function initAnimacaoScroll() {
+  const sections = document.querySelectorAll(".js-scroll");
+  if (sections.length) {
+    const windowMetade = window.innerHeight * 0.6;
+
+    function animaScroll() {
+      sections.forEach((section) => {
+        const sectionTop = section.getBoundingClientRect().top - windowMetade;
+        if (sectionTop < 0) {
+          section.classList.add("ativo");
+        } else {
+          section.classList.remove("ativo");
+        }
+      });
+    }
+  }
+  animaScroll();
+  window.addEventListener("scroll", animaScroll);
+}
+initAnimacaoScroll()
